a50408b704889f277ec51c70d67685e5e724ebab
AnthonyAscione <46875205+AnthonyAscione@users.noreply.github.com>
2019-01-27 15:32:54 -0600

    Fixed Crosshair

---
+++ Creative Project/Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationsPerMinute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 6.0f * rotationsPerMinute * Time.deltaTime);    }
+}
