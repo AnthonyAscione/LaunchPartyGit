6879bcb6c6a1face49e62af4a5b6aea3c8dc7681
AnthonyAscione <46875205+AnthonyAscione@users.noreply.github.com>
2019-09-25 20:09:11 -0500

    Summer Update!

--- Creative Project/Assets/Scripts/StarCollider.cs
+++ Creative Project/Assets/Scripts/StarCollider.cs
@@ -11,7 +11,8 @@ public class StarCollider : MonoBehaviour
     bool collided = false;
     GameObject StarManag;
     ShootingStar ss;
-   
+    public GameObject starDrop;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,74 +24,96 @@ public class StarCollider : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        Vector2 moveDirection = GetComponent<Rigidbody2D>().velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle =   Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+
         //Destroy(col.gameObject);
-        if(t == "Damage" && !collided){
+        //if (t == "Damage" && !collided)
+        //{
             collided = true;
             Damage(col.gameObject.name); // what it collided with; hopefully player 1 or player 2
-            //print("Red star collided");
-        }
+                                         //print("Red star collided");
+        //}
 
-        else if(t == "Invince" && !collided){
+        //else if (t == "Invince" && !collided)
+        //{
+        //    collided = true;
+        //    if (col.gameObject.name == "standard(Clone)")
+        //    {
+        //        Gain(col.gameObject.GetComponent<Bullet>().getOrigin());
+        //    }
+
+        //    else
+        //    {
+        //        Gain(col.gameObject.name);
+        //    }
+
+        //    //print("Green star collided");
+        //}
+
+        //else if (t == "Party")
+        //{
+        //    if (col.gameObject.name == "standard(Clone)" && !collided)
+        //    {
+        //        collided = true;
+        //        StarManag.GetComponent<ShootingStar>().diffStars = 1;
+        //        StarManag.GetComponent<ShootingStar>().delayTime = 0;
+        //        StarManag.GetComponent<ShootingStar>().speed *= 3;
+        //        //Debug.Log("storm initial");
+        //        StarManag.GetComponent<ShootingStar>().Signal();
+        //        //print("storm complere");
+        //        //StarManag.GetComponent<ShootingStar>().delayTime = 6;
+        //    }
+        //}
+
+        //else if (t == "Secondary" && !collided)
+        //{
+        //    collided = true;
+
+        //    if (col.gameObject.name == "standard(Clone)")
+        //    {
+        //        string orig = col.gameObject.GetComponent<Bullet>().getOrigin();
+        //        string targ = "";
+        //        if (orig == "Player1")
+        //        {
+        //            targ = "Player2";
+        //        }
+        //        else
+        //        {
+        //            targ = "Player1";
+        //        }
+
+        //        TakeAway(targ);
+        //    }
+
+        //    //If the star hits the player directly
+        //    else
+        //    {
+        //        TakeAway(col.gameObject.name);
+        //    }
+        //}
+
+        //else if(t == "Guns" && !collided)
+        //{
+            Instantiate(starDrop, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
             collided = true;
-            if (col.gameObject.name == "standard(Clone)"){
-                Gain(col.gameObject.GetComponent<Bullet>().getOrigin());
-            }
-
-            else{
-                Gain(col.gameObject.name);
-            }
-
-            //print("Green star collided");
-        }
-
-        else if(t == "Party"){
-            if (col.gameObject.name == "standard(Clone)" && !collided)
-            {
-                collided = true;
-                StarManag.GetComponent<ShootingStar>().diffStars = 1;
-                StarManag.GetComponent<ShootingStar>().delayTime = 0;
-                StarManag.GetComponent<ShootingStar>().speed *= 3;
-                //Debug.Log("storm initial");
-                StarManag.GetComponent<ShootingStar>().Signal();
-                //print("storm complere");
-                //StarManag.GetComponent<ShootingStar>().delayTime = 6;
-            }
-        }
-
-        else if(t == "Secondary" && !collided){
-            collided = true;
-
-            if(col.gameObject.name == "standard(Clone)"){
-                string orig = col.gameObject.GetComponent<Bullet>().getOrigin();
-                string targ = "";
-                if(orig == "Player1"){
-                    targ = "Player2";
-                }
-                else{
-                    targ = "Player1";
-                }
-
-                TakeAway(targ);
-            }
-
-            //If the star hits the player directly
-            else{
-                TakeAway(col.gameObject.name);
-            }
-        }
-
+        //}
 
         Destroy(this.gameObject);
-        
+
 
     }
 
-    void Damage(string n){
+    void Damage(string n)
+    {
         if (n == "Player1" || n == "Player2")
         {
             GameObject player = GameObject.Find(n);
@@ -99,7 +122,8 @@ public class StarCollider : MonoBehaviour
         }
     }
 
-    void Gain(string n){
+    void Gain(string n)
+    {
         //print("tried " + n);
         if (n == "Player1" || n == "Player2")
         {
@@ -108,10 +132,12 @@ public class StarCollider : MonoBehaviour
             h.GainHealth(50);
         }
     }
-    
 
-    void TakeAway(string n){
-        if(n == "Player1" || n == "Player2"){
+
+    void TakeAway(string n)
+    {
+        if (n == "Player1" || n == "Player2")
+        {
             GameObject player = GameObject.Find(n);
             GameObject child = player.transform.GetChild(4).gameObject;
             GameObject ch = player.GetComponent<Crosshair>().crosshair;
@@ -122,3 +148,4 @@ public class StarCollider : MonoBehaviour
     }
 
 }
+
